18cf91b2ba76d677791b093b018e0be81d4b603a
Linus-Smith <chyangbast@yeah.net>
2016-10-14 00:21:45 +0800

    add project

---
+++ app/src/main/java/com/chyang/android_rehearsal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.chyang.android_rehearsal;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
