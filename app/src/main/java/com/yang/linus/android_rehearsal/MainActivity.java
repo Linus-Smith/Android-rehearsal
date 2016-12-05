4183969ed951a5edc1c236b6bca3ed09f37dc4c6
Linus-Smith <chyangbast@yeah.net>
2016-12-05 14:53:34 +0800

    [DOME][AR] 初始化DOME

---
+++ app/src/main/java/com/yang/linus/android_rehearsal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yang.linus.android_rehearsal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
