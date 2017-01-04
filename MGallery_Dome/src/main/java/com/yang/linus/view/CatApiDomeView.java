de03969c718e32b3f5c628fe01ab0dc02ddb7fdf
Linus-Smith <chyangbast@yeah.net>
2017-01-04 23:56:18 +0800

    [AM] add setCoolorFilter　dome

--- MGallery_Dome/src/main/java/com/yang/linus/view/CatApiDomeView.java
+++ MGallery_Dome/src/main/java/com/yang/linus/view/CatApiDomeView.java
@@ -1,6 +1,10 @@
 package com.yang.linus.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -11,27 +15,43 @@ import android.view.View;
 
 public class CatApiDomeView extends View {
 
+    private Paint mPaint;
+
     public CatApiDomeView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public CatApiDomeView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public CatApiDomeView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mPaint = new Paint();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        System.out.println("onMeasure");
+
     }
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
         System.out.println("onLayout====");
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        ColorMatrix colorMatrix = new ColorMatrix(new float[] {
+            1, 0, 0, 0, 0,
+            0, 1, 0, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 0, 1, 0,
+        }) ;
+
+        mPaint.setColorFilter(new ColorMatrixColorFilter((colorMatrix)));
     }
 }
