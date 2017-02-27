8a034a81b7e010a0dcaaae650aeb2ede0df0f784
Linus-Smith <chyangbast@yeah.net>
2017-02-27 15:05:34 +0800

    add fragment Dome

--- MDevelopers/src/main/java/com/yang/mdevelopers/activity/FragmentDomeActivity.java
+++ MDevelopers/src/main/java/com/yang/mdevelopers/activity/FragmentDomeActivity.java
@@ -1,7 +1,15 @@
 package com.yang.mdevelopers.activity;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.yang.mdevelopers.R;
 
@@ -11,5 +19,18 @@ public class FragmentDomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_dome);
+
+        FragmentManager  fragmentManager = getSupportFragmentManager();
+        FragmentTransaction mFragmentTransaction = fragmentManager.beginTransaction();
+
+    }
+
+    class FragmentOne extends Fragment {
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return super.onCreateView(inflater, container, savedInstanceState);
+        }
     }
 }
