ef4aac07e718dd9bdff8008155aa6260ddb108f7
Linus-Smith <chyangbast@yeah.net>
2017-04-24 13:45:56 +0800

    add

--- MArithmetic/src/main/java/com/chyang/marithmetic/MainActivity.java
+++ MArithmetic/src/main/java/com/chyang/marithmetic/MainActivity.java
@@ -1,14 +1,30 @@
 package com.chyang.marithmetic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.chyang.marithmetic.activity.SingleNodeActivity;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.bt_single_node).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent mIntent = new Intent();
+        switch (v.getId()) {
+            case R.id.bt_single_node:
+                mIntent.setClass(this, SingleNodeActivity.class);
+                break;
+        }
+        startActivity(mIntent);
     }
 }
--- MArithmetic/src/main/java/com/chyang/marithmetic/activity/SingleNodeActivity.java
+++ MArithmetic/src/main/java/com/chyang/marithmetic/activity/SingleNodeActivity.java
@@ -2,14 +2,66 @@ package com.chyang.marithmetic.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.chyang.marithmetic.R;
 
-public class SingleNodeActivity extends AppCompatActivity {
+import java.util.LinkedList;
+import java.util.Random;
+
+public class SingleNodeActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    class NodeObj {
+        int num;
+        NodeObj next;
+    }
+
+    private NodeObj  mNodeHead;
+    private NodeObj  mNodeEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_node);
+        findViewById(R.id.bt_add).setOnClickListener(this);
+        findViewById(R.id.bt_print).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_add:
+                NodeObj obj = new NodeObj();
+                obj.num = new Random().nextInt();
+                addNode(obj);
+                break;
+            case R.id.bt_print:
+                printNode();
+                break;
+        }
+    }
+
+
+    public void addNode(NodeObj nodeObj) {
+        if(mNodeHead == null) {
+            mNodeHead = nodeObj;
+            mNodeEnd = mNodeHead;
+        } else {
+            mNodeEnd.next = nodeObj;
+            mNodeEnd = nodeObj;
+        }
+    }
+
+    public void printNode() {
+        NodeObj printNode;
+        if(mNodeHead != null) {
+            printNode = mNodeHead;
+           do{
+               System.out.println(printNode.num+"========");
+               printNode = printNode.next;
+           } while(printNode.next != null);
+        }
     }
 }
