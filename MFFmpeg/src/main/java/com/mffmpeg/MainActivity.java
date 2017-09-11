c2728486e809be6c96dcc3dda5e8cd67be0e9815
Liuns-Smith <chyangbast@yeah.net>
2017-09-12 00:45:08 +0800

    add lib

--- MFFmpeg/src/main/java/com/mffmpeg/MainActivity.java
+++ MFFmpeg/src/main/java/com/mffmpeg/MainActivity.java
@@ -5,9 +5,23 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("avcodec");
+        System.loadLibrary("avformat");
+        System.loadLibrary("avutil");
+        System.loadLibrary("swresample");
+        System.loadLibrary("swscale");
+
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      System.out.println(stringFromJNI());
     }
+
+    public native String stringFromJNI();
 }
